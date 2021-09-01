f3342c893e33683b5ad3ebc673b8776a069804ec
lucaslmeireles <86924068+lucaslmeireles@users.noreply.github.com>
2021-09-01 16:15:25 -0300

    mias

--- agenda/settings.py
+++ agenda/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'adminuninter',
+        'PASSWORD': 'curs0Pyth0n@169',
+        'HOST': '127.0.0.1',
+        'NAME': 'uninter'
     }
 }
 
